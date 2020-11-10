42a23a8743be2b75a939cd85d9d6a90f2d9c26bb
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-11-09 20:20:48 -0500

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,16 @@
+world.on('M', function (crumb) {
+    const prefix = "!"
+    let msg = crumb.m.toLowerCase()
+
+    if (!msg.startsWith(prefix)) return
+
+    let args = msg.slice(prefix.length).trim().split(/ +/)
+    let cmd = args.shift()
+
+    if (cmd == "ping") {
+        world.message("Pong!")
+    }
+})
+
+ // TODO:
+ // Make it so the balloon doesn't show up when you send a custom command. (Like the real commands)
