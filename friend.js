59272a640ed7a63b4f2d07dc3673f5da9d77b239
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-11-12 00:36:32 -0500

    Update friend.js

--- friend.js
+++ friend.js
@@ -11,8 +11,9 @@ world.on('M', function (crumb) {
 
     if (cmd == "friend") {
         let user = common.findObjectInArray("nickname", args[0], world.players)
-        let alert = new boot.Alert(300, 10, "Sent a friend request to " + user.nickname + ".")
-                .centerTo(world.stage.boot, 'top').bump(0, 20);
-world.stage.boot.addChild(alert);
+        
+        world.emit("alert", {
+            message: `Sent a friend request to ${user.nickname}`})
+        }
     }
 })
